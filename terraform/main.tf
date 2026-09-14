bda0d1821aefe7a4d0698da964013e3f6cf7192f
ah <adrianihernandez@protonmail.com>
2026-09-13 21:16:31 -0500

    Provision k3s worker VMs via Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ resource "proxmox_virtual_environment_vm" "k3s_cp01" {
     bridge = "vmbr0"
   }
 }
+
+resource "proxmox_virtual_environment_vm" "k3s_workers" {
+  count     = 3
+  name      = "k3s-wk0${count.index + 1}"
+  node_name = "proxmox"
+  vm_id     = 102 + count.index
+
+  clone {
+    vm_id = 100
+    full  = true
+  }
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 8192
+  }
+
+  agent {
+    enabled = true
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+}
